19df47f0eb11d3ef59398b9da065c93529d9b848
Roman Ryzhikov <romansmerdyakov@gmail.com>
2024-09-04 18:58:09 +0500

    feat(counter): :fire: add counter
    Implemented counter component. It has two method as like inc and dec
    
    awazing, awesome, super

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>App</h1>
+     <h1>Counter: {count}</h1>
+     <button onClick={() => setCount((prev) => prev - 1)}>DEC</button>
+     <button onClick={() => setCount((prev) => prev + 1)}>INC</button>
     </>
   )
 }
